a4e48c34516351fd09b8f2d034008f3253414fb0
akash-12398 <napinderbajwa@gmail.com>
2020-08-26 14:25:41 -0500

    First Commit

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -11,31 +11,43 @@ namespace IterationStatements
 
             //Create a List called "numbers" - DONE
             var numbers = new List<int>();
-            
+
             //Create a variable of type int and initialize the variable with a value of 0
+            var myNumber = 0;
+
+
+            do
+            {
+                myNumber++;  //100
+
+                numbers.Add(myNumber); //100
+
+            } while (myNumber < 100);
 
 
 
-            // Create a do-while loop
+
+            while (myNumber < 200)
+
             {
                 // Increment your variable by 1
+                myNumber++;
                 // Then add your variable to "numbers"
+                numbers.Add(myNumber);
+
             }
-            // While your variable is less than 100
 
 
-
-            // Create a while loop
-            // While your variable is less than 200
-            {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
-            }
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
             // Write your variable to the console
+            foreach(var item in numbers)
+            {
+
+                Console.WriteLine(item);
+            }
 
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
@@ -45,8 +57,10 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for(int i = 199; i <= numbers.Count && i >=0; i--) 
             {
-                // Write to the console "numbers" at index i
+                Console.WriteLine($"{numbers[i]}");
+                
             }
         }
     }
